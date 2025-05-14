97de5f03d852b9e40269c0fc88eab69dc2f3aa7e
Harshith KV <harshith.shetty2002@gmail.com>
2025-05-14 14:58:44 +0530

    Learning

---
+++ frontend/src/components/greet/greet.test.tsx
@@ -0,0 +1,17 @@
+// Tdd approach
+
+import { render, screen } from "@testing-library/react"
+import Greet from "./greet"
+
+
+test('Greet renders correctly', () => {
+    render(<Greet/>)
+    const textElement = screen.getByText('Hello')
+    expect(textElement).toBeInTheDocument()
+})
+
+test('Greet renders with a name', () => {
+    render (<Greet name= 'Rakshith' />)
+    const textElement = screen.getByText('Hello Rakshith')
+    expect(textElement).toBeInTheDocument()
+})
