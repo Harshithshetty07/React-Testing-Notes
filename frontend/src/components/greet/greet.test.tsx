5c04db034ccef87c1d20ae6436d9586683941c0b
Harshith KV <harshith.shetty2002@gmail.com>
2025-05-15 12:01:50 +0530

    Learning on

--- frontend/src/components/greet/greet.test.tsx
+++ frontend/src/components/greet/greet.test.tsx
@@ -3,15 +3,50 @@
 import { render, screen } from "@testing-library/react"
 import Greet from "./greet"
 
-
-test('Greet renders correctly', () => {
+describe('Greet', () => {
+test('renders correctly', () => {
     render(<Greet/>)
     const textElement = screen.getByText('Hello')
     expect(textElement).toBeInTheDocument()
 })
 
-test('Greet renders with a name', () => {
+describe('Nested', () => {
+test('renders with a name', () => {
     render (<Greet name= 'Rakshith' />)
     const textElement = screen.getByText('Hello Rakshith')
     expect(textElement).toBeInTheDocument()
 })
+})
+
+})
+
+
+
+/* JEST it Method */
+
+// describe('Greet', () => {
+// it('renders correctly', () => {
+//     render(<Greet/>)
+//     const textElement = screen.getByText('Hello')
+//     expect(textElement).toBeInTheDocument()
+// })
+
+// describe('Nested', () => {
+// fit('renders with a name', () => {
+//     render (<Greet name= 'Rakshith' />)
+//     const textElement = screen.getByText('Hello Rakshith')
+//     expect(textElement).toBeInTheDocument()
+// })
+// })
+
+// })
+
+
+
+/* JEST Watch mode */ 
+
+// test.skip('Greet renders with a name', () => {
+//     render (<Greet name= 'Rakshith' />)
+//     const textElement = screen.getByText('Hello Rakshith')
+//     expect(textElement).toBeInTheDocument()
+// })
